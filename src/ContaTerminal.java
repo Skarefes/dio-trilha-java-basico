41770cd358b84c6962f5c68b2c6840ee230d7998
Roger Yamassaki <skarefes@gmail.com>
2025-02-25 18:00:44 -0300

    primeiro commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Por favor, digite o numero da Agência!");
+
+
+
+    }
+}
